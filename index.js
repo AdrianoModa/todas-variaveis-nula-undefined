31663f768ae31185bd4d0069892a34983c6602ec
AdrianoModa <adrianomodafeitosa@gmail.com>
2020-12-28 12:46:39 -0300

    recebendo valores em campos (input)

--- index.js
+++ index.js
@@ -1,44 +1,46 @@
-// Import stylesheets
+//Import stylesheets
 import "./style.css";
 
 /**
-  * Verifica se todas as variaveis informadas retornam `null` ou `undefined`.
-  * @param variables um spread que aloca as variaveis a serem testadas.
-  * @author `Adriano Moda`
-*/
-const isDivTrue = document.getElementById("exibeSeVerdadeiro");
-const isDivFalse = document.getElementById("exibeSeFalso");
-const valueABC = document.getElementById("valueABC");
-const valueDEF = document.getElementById("valueDEF");
-const a = null;
-const b = undefined;
-const c = null;
-var arrayABC = [a,b,c];
-var txtABC = "";
-const d = 'Gandalf';
-const e = 'Bilbo';
-const f = 'Frodo';
-var arrayDEF = [d,e,f];
-var txtDEF = "";
-isDivTrue.innerHTML = isNullOrUndefinedWithArray(a, b, c);
-isDivFalse.innerHTML = isNullOrUndefinedWithArray(d, e, f);
+ * Verifica se todas as variaveis informadas retornam `null` ou `undefined`.
+ * @param variables um spread que aloca as variaveis a serem testadas.
+ * @author `Adriano Moda`
+ */
 
-arrayABC.forEach(displayArrayValuesABC)
-document.getElementById("valor-abc").innerHTML = txtABC
-
-arrayDEF.forEach(displayArrayValuesDEF)
-document.getElementById("valor-def").innerHTML = txtDEF
+const resultadoDocument = document.getElementById("resultado");
+const buttonResult = document.getElementById("btn-resultado");
 
 function isNullOrUndefinedWithArray(...variables) {
   const listObjects = [...variables];
   const isNullOrUndefined = listObjects.every(f => f == undefined || null);
-  return isNullOrUndefined
+  return isNullOrUndefined;
 }
 
-function displayArrayValuesABC(value){
-  txtABC = txtABC + value + ", ";
-}
+buttonResult.onclick = function() {
+  var valor_1_target = document.querySelector("#input-valor-1");
+  var valor_2_target = document.querySelector("#input-valor-2");
+  var valor_3_target = document.querySelector("#input-valor-3");
 
-function displayArrayValuesDEF(value){
-  txtDEF = txtDEF + value + ", ";
-}
+  if (valor_1_target.value == "") {
+    valor_1_target = null;
+  }
+
+  if (valor_2_target.value == "") {
+    valor_2_target = null;
+  }
+
+  if (valor_3_target.value == "") {
+    valor_3_target = null;
+  }
+
+  return (resultadoDocument.innerHTML =
+    "São todas nulas? " +
+    isNullOrUndefinedWithArray(valor_1_target, valor_2_target, valor_3_target));
+};
+
+var buttonReset = document.getElementById("btn-reset");
+buttonReset.onclick = function() {
+  document.getElementById("input-valor-1").value = "";
+  document.getElementById("input-valor-2").value = "";
+  document.getElementById("input-valor-3").value = "";
+};
